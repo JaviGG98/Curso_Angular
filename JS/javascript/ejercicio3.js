36f5faa080a1e2a6bf31f413eca4c3d11b0abdbf
Javier García <javiergarciaguardiola98@gmail.com>
2019-04-09 13:39:30 +0200

    ejercicio3 terminado

--- JS/javascript/ejercicio3.js
+++ JS/javascript/ejercicio3.js
@@ -1,1 +1,11 @@
 'use strict'
+let num = parseInt(prompt("Introduce un numero"));
+
+let ar = new Array();
+for (let i = 1; i <= num; i++) {
+    ar.push(i);
+}
+ar.push(num);
+ar.shift();
+console.log(ar.filter(num => num%2==0));
+console.log(ar.map(num => num*2));
