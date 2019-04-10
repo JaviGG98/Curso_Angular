7fab628ac8099ddeb22f63b73aa6dd07b353c3c0
Javier García <javiergarciaguardiola98@gmail.com>
2019-04-10 13:09:27 +0200

    eventos js

--- JS/javascript/dom3.js
+++ JS/javascript/dom3.js
@@ -4,11 +4,15 @@ var divEncabezado = document.getElementById("encabezadoFactura");
 divEncabezado.classList.remove("encabezado");
 divEncabezado.classList.add("resalta");
 
-function incremento() 
-{
-   let num = divEncabezado.getElementsByTagName("label")[2].innerHTML;
-   let total = parseInt(num)+1; 
-   divEncabezado.getElementsByTagName("label")[2].innerHTML = total;  
-   console.log(total); 
+function incremento() {
+    // OTRAS FORMAS DE SACAR EL HIJO DEL DIV
+    // --------------------------------------------------//
+    // let hijo = document.getElementByClassName("numeroFactura")[0]; //
+    // let hijo = divEncabezado.children[2]; //
+    // ---------------------------------------------------//
+    let num = divEncabezado.getElementsByTagName("label")[2].innerHTML;
+    let total = parseInt(num) + 1; // hijo.innerHTML = hijo.innerHTML + 1;
+    divEncabezado.getElementsByTagName("label")[2].innerHTML = total;
+    console.log(total);
 }
 incremento();
