06a367bd004c8545783ed0b400aedb741acaff12
Javier García <javiergarciaguardiola98@gmail.com>
2019-04-15 14:25:26 +0200

    importante echarle un ojo en casa a servidor.js

--- JS/proyectoNode/ejercicios/Tema1_EnrutamientoBasico/servidor.js
+++ JS/proyectoNode/ejercicios/Tema1_EnrutamientoBasico/servidor.js
@@ -3,46 +3,41 @@ const fs = require('fs');
 const os = require("os");
 const link = require("url");
 
-var atenderPeticion = (request,response) => 
-{
-    var user = os.hostname();
-    var fichero = os.homedir();
-    if (request.url == '/') 
-    {
-        response.writeHead(200,{'Content-Type':'text/html'});
-        fs.readFileSync('./index.html','utf-8');
+var atenderPeticion = (request, response) => {
+    var usuario = os.hostname();
+    if (request.url == '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(fs.readFileSync('./index.html', 'utf-8'));
     }
-    else if (request.url == './usuario')
-    {
-        response.writeHead(200,{'Content-Type':'text/plain'});
-        response.write(user);
+    else if (request.url == '/usuario') {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write(usuario);
 
     }
-    else if (request.url == './carpeta')
-    {
-        response.writeHead(200,{'Content-Type':'text/plain'});
-        fs.readdirSync('.').foreach(fichero  => {response.write(fichero + "\n")});
+    else if (request.url == "/carpeta") {
+        console.log("hola");
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        fs.readdirSync(".").forEach(fichero => {
+            response.write(fichero + "\n");
+        });
     }
-    else if(request.url !='/' | request.url != './carpeta' )
-    {
-        response.writeHead(200,{'Content-Type':'image/png'});
-        fs.readFileSync("./5108.png_860.png");
-    }
-    else if(request.url.starsWith('/saludo'))
-    {
-        response.writeHead(200,{'Content-Type':'text/plain'});
+    // else if(request.url !='/' | request.url != './carpeta' )
+    // {
+    //     response.writeHead(200,{'Content-Type':'image/png'});
+    //     response.write(fs.readFileSync("./5108.png_860.png"));
+    // }
+    else if (request.url.startsWith('/saludo')) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
         var partes = request.url.split('/');
         var nombre = partes[2];
         var edad = partes[3];
-        response.write("Hola, "+nombre+" tienes "+edad+" años")
-        if (edad < 18)
-        {
-            response.writeHead(200,{'Content-Type':'text/plain'});
+        response.write("Hola, " + nombre + " tienes " + edad + " años")
+        if (edad < 18) {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
             response.write("Eres menor de edad");
         }
-        else
-        {
-            response.writeHead(200,{'Content-Type':'text/plain'});
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
             response.write("Eres mayor de edad");
         }
     }
